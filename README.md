f8e45372540dc1a09603d861cbd5cc47088b0d72
Jackie <test@test.test>
2023-01-31 23:58:52 +0600

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+## Simple Django project template with pre-settings, custom user model and required packages
+
+> Don't forget to add .env file with variables
---
+++ core/settings/prod.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["127.0.0.1"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_NAME"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("POSTGRES_HOST"),
+        "PORT": config("POSTGRES_PORT"),
+    }
+}
